38cbf6b6ac10b25523cf6eede1ed7a9f5d9f0d64
hiten14 <hjsmwdev@gmail.com>
2022-10-13 02:56:17 +0530

    init commit

---
+++ src/main/java/com/example/demo/dao/PersonDataAccessService.java
@@ -0,0 +1,57 @@
+// package com.example.demo.dao;
+package com.example.demo.dao;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Person;
+
+@Repository("postgresql")
+public class PersonDataAccessService implements PersonDao {
+
+    private static List<Person> DB = new ArrayList<>();
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        
+        DB.add(new Person(id, person.getName()));
+        return 1;
+    }
+    @Override
+    public List<Person> selectAllPeople() {
+        
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return DB.stream()
+                .filter(person -> person.getId().equals(id))
+                .findFirst();
+    }
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> person = selectPersonById(id);
+        if (person.isEmpty()){
+            return 0;
+        }
+        DB.remove(person.get());
+        return 1;
+    }
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return selectPersonById(id).map(oldP -> {
+            int index = DB.indexOf(oldP);
+            if (index >= 0){
+                DB.set(index, new Person(id, person.getName()));
+                return 1;
+            }
+            else
+                return 0;
+        })
+        .orElse(0);
+    }
+}
